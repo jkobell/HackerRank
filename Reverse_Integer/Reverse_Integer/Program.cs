c3d1d923120e3e69e36a2f47860c47cb43593233
jkobell <k4kobell@gmail.com>
2019-05-11 05:42:26 -0500

    initial commit

---
+++ Reverse_Integer/Reverse_Integer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Integer
+{
+    class Program
+    {
+        static int[] reverseArray(int[] a)
+        {
+            int arrLen = a.Length;
+            int[] result = new int[arrLen];
+            int j = arrLen - 1;
+            for (int i = 0; i < arrLen; i++)
+            {
+                result[i] = a[j];
+                j--;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number to fill array.");
+
+            string strRead = Console.ReadLine();
+            char[] strArr = strRead.ToCharArray();
+            int[] arr1 = strArr.Select(x => ((int)x) - '0').ToArray(); //subtracts 48 from the ASCII value of each
+
+            int[] res = reverseArray(arr1);
+            foreach (int pr in res)
+            {
+                Console.Write(pr);
+            }
+            Console.ReadKey();
+        }
+    }
+}
